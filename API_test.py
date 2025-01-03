4f1a99d36781bd0e89b19a7ab239e973c13dccec
Vadim Burin <burinvad@mail.ru>
2025-01-03 20:32:54 +0300

    add mock

--- API_test.py
+++ API_test.py
@@ -1,18 +1,37 @@
 import os
 import requests
+from unittest.mock import patch
 from dotenv import load_dotenv
 
 load_dotenv('.env')
 
 API_KEY = os.getenv('API_KEY')
 
-response = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q=Moscow&appid={API_KEY}')
+def get_coords(city: str) -> tuple:
+    """Получение координат по названию города"""
 
-lat = response.json()[0]['lat']
-lon = response.json()[0]['lon']
+    response = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q=Moscow&appid={API_KEY}')
 
-print(lat, lon)
+    lat = response.json()[0]['lat']
+    lon = response.json()[0]['lon']
 
-response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}')
+    return lat, lon
 
-print(response.json())
+
+def get_weather(lat: float, lon: float) -> float:
+    """Получение погоды по координатам"""
+
+    response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}')
+
+    return response.json()['main']['temp']
+
+
+@patch('requests.get')
+def test_get_weather(mock_get):
+    mock_get.return_value.json.return_value = {'main': {'temp': 1}}
+    assert get_weather(1, 1) == 1
+    mock_get.assert_called_once_with(f'https://api.openweathermap.org/data/2.5/weather?lat=1&lon=1&appid={API_KEY}')
+
+if __name__ == '__main__':
+    lat, lon = get_coords('Moskow')
+    print(get_weather(lat, lon))
